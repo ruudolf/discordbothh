8ef1ed1042e8507e61977b8beca000a21e6477b8
ruudolf <33085126+ruudolf@users.noreply.github.com>
2020-05-12 09:55:58 +0300

    Add files via upload
    error message palautus olemattomille komennoille ja pienta cleanuppia

--- bot.py
+++ bot.py
@@ -24,18 +24,9 @@ async def reload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
     bot.load_extension(f'cogs.{extension}')
     
-@bot.event
-async def on_member_join(member):
-    print(f'{member} has joined the server.')
-
-@bot.event
-async def on_member_remove(member):
-    print(f'{member} has left a server.')
-
 #helpotusta cogien kutsumiseen
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
-
 
 bot.run('')
